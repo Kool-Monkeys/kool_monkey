01a865ca33eec37ae589ee77955a25424901603e
Sergio Visinoni <piffio@piffio.org>
2016-02-01 11:55:47 -0600

    Create a container for PostgreSQL 9.5
    We can now initialize a container running PostgreSQL 9.5 with the
    up-to-date schema.
    
    As of now the operation is destructuve, meaning that the DB will
    be re-initialized at every startup.
    
    Fixes #14

--- scripts/db/02_create_schema.sql
+++ scripts/db/02_create_schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS testAgent;
 DROP TABLE IF EXISTS test;
 DROP TABLE IF EXISTS agents;
 
-CREATE TABLE agent(
+CREATE TABLE agent (
 		id SERIAL PRIMARY KEY,
 		ip cidr NOT NULL,
 		last_alive timestamp NOT NULL DEFAULT now()
@@ -14,7 +14,7 @@ GRANT SELECT ON agent TO kool_reader;
 GRANT SELECT,INSERT,DELETE,UPDATE ON agent_id_seq TO kool_writer;
 GRANT SELECT ON agent_id_seq TO kool_reader;
 
-CREATE TABLE test(
+CREATE TABLE test (
 		id SERIAL PRIMARY KEY,
 		targetUrl VARCHAR(512) NOT NULL,
 		frequency INTEGER NOT NULL default 30
@@ -25,7 +25,7 @@ GRANT USAGE,SELECT ON SEQUENCE test_id_seq TO kool_writer;
 GRANT SELECT ON test TO kool_reader;
 GRANT SELECT ON test_id_seq TO kool_reader;
 
-CREATE TABLE testAgent(
+CREATE TABLE testAgent (
 		idAgent INTEGER NOT NULL REFERENCES agent(id),
 		idTest INTEGER NOT NULL REFERENCES test(id),
 		PRIMARY KEY (idAgent, idTest)
@@ -34,7 +34,7 @@ CREATE TABLE testAgent(
 GRANT SELECT,INSERT,DELETE,UPDATE ON testAgent TO kool_writer;
 GRANT SELECT ON testAgent TO kool_reader;
 
-CREATE TABLE result(
+CREATE TABLE result (
 		id SERIAL PRIMARY KEY,
 		agent_id INTEGER NOT NULL REFERENCES agent(id),
 		test_id INTEGER NOT NULL REFERENCES test(id),
@@ -47,5 +47,3 @@ GRANT SELECT,INSERT,DELETE,UPDATE ON result TO kool_writer;
 GRANT USAGE,SELECT ON SEQUENCE result_id_seq TO kool_writer;
 GRANT SELECT ON result TO kool_reader;
 GRANT SELECT ON result_id_seq TO kool_reader;
-
-INSERT INTO agent(ip) VALUES ('127.0.0.1');
--- scripts/db/create_db.sql
+++ scripts/db/create_db.sql
@@ -1,2 +1,2 @@
-\i %DB_SCRIPTS_DIR%/create_roles.sql
-\i %DB_SCRIPTS_DIR%/create_schema.sql
+\i %DB_SCRIPTS_DIR%/01_create_roles.sql
+\i %DB_SCRIPTS_DIR%/02_create_schema.sql
