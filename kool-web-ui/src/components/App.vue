760bc8351c18b7d314d3504ce14db3c9095c9567
Sergio Visinoni <piffio@piffio.org>
2016-02-14 19:37:24 -0600

    Get the Navbat right
    Now we have a navbar that is more or les usable.
    There are still some glitches that need to be fixed, but the overall
    behavior is there.

--- kool-web-ui/src/components/App.vue
+++ kool-web-ui/src/components/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="wrapper">
     <nav class="navbar navbar-default">
-      <div class="container">
-        <ul class="nav navbar-nav">
-          <li><a v-link="'home'">Home</a></li>
-          <li><a v-link="'dashboard'" v-show="authenticated">Dashboard</a></li>
-          <li><a v-link="'settings'" v-show="authenticated">Settings</a></li>
-          <li><a v-link="'#'" @click="login()" v-show="!authenticated">Login</a>
-              <a v-link="'#'" @click="logout()" v-show="authenticated">Logout</a></li>
-        </ul>
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-items-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" v-link="'home'"><img alt="Kool Monkey" src="../../img/kool-monkey-logo-header.png"></a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbar-collapse-items-1">
+          <ul class="nav navbar-nav navbar-left">
+            <li><a v-link="'dashboard'" v-show="authenticated">Dashboard</a></li>
+            <li><a v-link="'settings'" v-show="authenticated">Settings</a></li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a v-link="'#'" @click="login()" v-show="!authenticated">Login</a>
+                <a v-link="'#'" @click="logout()" v-show="authenticated">Logout</a></li>
+          </ul>
+        </div>
       </div>
     </nav>
     <div class="container">
--- kool-web-ui/src/components/Home.vue
+++ kool-web-ui/src/components/Home.vue
@@ -1,7 +1,21 @@
 <template>
-  <h1>Welcome to Kool Monkey</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Welcome to Kool Monkey</h1>
+      </div>
+    </div>
 
-  <div id="text">
-  Something something about Kool Monkey.
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <img class="img-responsive" src="../../img/kool-monkey-logo.png">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12" id="text">
+        This is cool Monkey
+      </div>
+    </div>
   </div>
 </template>
